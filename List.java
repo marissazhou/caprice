79c040d1c81d940a00c4d51011e599f5c4150d56
marissazhou <marissa.zhou.cn@gmail.com>
2014-10-15 11:21:45 +0100

    Add List Class
    List class is for all list related manipulations

---
+++ List.java
@@ -0,0 +1,73 @@
+/* Created by   :   Lijuan Marissa Zhou
+   Email        :   marissa.lala.joo@gmail.com 
+   Time         :   1/10/2014
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.EmptyStackException;
+
+public class List{
+
+    /**
+     * Definition for singly-linked list with a random pointer.
+     */
+    class RandomListNode {
+        int label;
+        RandomListNode next, random;
+        RandomListNode(int x) { this.label = x; }
+    };
+
+    public static void main(String[] args) {
+        try {
+            if (args.length>0){
+                int[] A = new int[]{1,2,0,1,2,0,1,2,0,2,2,1,2,0,2,1,1,1,0};
+                sort3Colors(A);
+                for (int i=0; i<A.length; i++) { 
+                    System.out.print(A[i]);
+                    System.out.print(" ");
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    /** Copy List with Random Pointer
+      * 
+      * @param s the string to decode like '1235478' 
+      * @return how many ways to decode the string
+      * @see 
+      *
+    */
+    public static RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) return null;
+        RandomListNode nh = new RandomListNode(head.label);
+        HashMap<RandomListNode,RandomListNode> map = new HashMap<RandomListNode,RandomListNode>();
+        HashMap<RandomListNode,RandomListNode> mapR = new HashMap<RandomListNode,RandomListNode>();
+        RandomListNode p = nh;
+        mapR.put(head, head.random);
+        map.put(head, nh);
+        while (head.next != null) {
+            RandomListNode nxt = new RandomListNode(head.next.label);
+            p.next = nxt; 
+            mapR.put(head.next, head.next.random);
+            map.put(head.next, nxt);
+            head = head.next;
+            p = p.next;
+        }
+        for (RandomListNode cur : mapR.keySet()) {
+            map.get(cur).random = map.get(mapR.get(cur));
+        }
+        return nh;
+    }
+}
